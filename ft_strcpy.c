775623ff19727c6debc294d3ba65f3fa8727df16
Eunsu Ahn <theeunsu@gmail.com>
2024-07-02 22:40:34 +0200

    exam

---
+++ ft_strcpy.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+char	*ft_strcpy(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	s1[i] = '\0';
+	return (s1);
+}
+#include <stdio.h>
+
+int	main(void)
+{
+	char	s1[15];
+	char	s2[] = "Hello, World!";
+
+	printf("%s\n", ft_strcpy(s1, s2));
+}
+
+// Assignment name  : ft_strcpy
+// Expected files   : ft_strcpy.c
+// Allowed functions:
+// --------------------------------------------------------------------------------
+
+// Reproduce the behavior of the function strcpy (man strcpy).
+
+// Your function must be declared as follows:
+
+// char    *ft_strcpy(char *s1, char *s2);
