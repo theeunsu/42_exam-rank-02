1b51eb21eeed415f6d85cb770baef21029a20bbe
Eunsu <theeunsu@gmail.com>
2024-10-11 15:00:53 +0200

    Update ft_strrev.c

--- level1/ft_strrev.c
+++ level1/ft_strrev.c
@@ -1,27 +1,6 @@
 
-
 #include <stdio.h>
 
-// char	*ft_strrev(char *str)
-// {
-// 	int		i;
-// 	int		len;
-// 	char	tmp;
-
-// 	i = 0;
-// 	len = 0;
-// 	while (str[len] != '\0')
-// 		len++;
-// 	while (i < len - 1)
-// 	{
-// 		tmp = str[i];
-// 		str[i] = str[len - 1];
-// 		str[len - 1] = tmp;
-// 		i++;
-// 		len--;
-// 	}
-// 	return (str);
-// }
 
 char	*ft_strrev(char *str)
 {
